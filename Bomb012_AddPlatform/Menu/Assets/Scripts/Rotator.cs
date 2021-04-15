ea19204ceb16bebab93019d0ce23be0b1bf2980d
arashj <arashjamshidi12@gmail.com>
2021-04-15 00:08:55 -0400

    Menu_Scene

---
+++ Bomb012_AddPlatform/Menu/Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public float rotationSpeed = 250.0f;
+	
+	
+	void Update () {
+		
+		transform.Rotate(new Vector3(   0 , 0 ,  rotationSpeed * Time.deltaTime));
+	}
+
+}
